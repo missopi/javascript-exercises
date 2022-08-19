80c0f134e30dab3284fa12bc0884c2bb94bb3b04
missopi <missopi@me.com>
2022-08-19 15:02:31 +0100

    Edit code to fix function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num >= 0) {
+        return string.repeat(num);
+    }
+    else {
+        return 'ERROR';
+    }
 };
+
+repeatString('hey', 3);
+repeatString('hey', 10);
+repeatString('hey', 1);
+repeatString('hey', 0);
+repeatString('hey', -1);
+repeatString('hey', 17);
+repeatString('', 10);
 
 // Do not edit below this line
 module.exports = repeatString;
