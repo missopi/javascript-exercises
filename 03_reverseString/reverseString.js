3806429c5ec34eb8eb569b3eaf46108b61978474
missopi <missopi@me.com>
2022-08-19 20:49:38 +0100

    Complete function loop to return words in reverse

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newString = '';
+    for (let i = string.length - 1; i >= 0; i--) {
+        newString += string[i];
+    }  
+    return newString;
 };
+
+reverseString('hello');
+reverseString('helle there');
+reverseString('123! abc!');
+reverseString('');
 
 // Do not edit below this line
 module.exports = reverseString;
