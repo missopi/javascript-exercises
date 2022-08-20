388f087c1c6c70147f2f8e0adb3b190c460399c8
missopi <missopi@me.com>
2022-08-20 21:55:20 +0100

    Removes all values from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,7 @@
-const removeFromArray = function(array, value, value2) {
+const removeFromArray = function(array, value, value2, value3, value4) {
     let i = array.length - 1;
     while (i >= 0) {
- 	    if ([value, value2].indexOf(array[i]) > (-1)) {
+ 	    if ([value, value2, value3, value4].indexOf(array[i]) > (-1)) {
     	    array.splice(i, 1);
   	    }
   	    i -= 1;
