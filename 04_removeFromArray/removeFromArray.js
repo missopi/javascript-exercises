272092e4706e29cc6cbecd8b5bcf9af92b36bc4c
missopi <missopi@me.com>
2022-08-20 20:50:54 +0100

    Pass first test successfully

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, value) {
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] == value) {
+            array.splice(i, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
