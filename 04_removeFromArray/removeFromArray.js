025af6017b251acbe32d0e8b1ecfd7b634c49ecc
missopi <missopi@me.com>
2022-08-20 21:50:32 +0100

    Remove multiple values from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,14 @@
-const removeFromArray = function(array, value) {
-    for(let i = 0; i < array.length; i++) {
-        if(array[i] == value) {
-            array.splice(i, 1);
-        }
+const removeFromArray = function(array, value, value2) {
+    let i = array.length - 1;
+    while (i >= 0) {
+ 	    if ([value, value2].indexOf(array[i]) > (-1)) {
+    	    array.splice(i, 1);
+  	    }
+  	    i -= 1;
     }
     return array;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
