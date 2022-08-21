17ca60208d3c53cf168e45b9468c528e94db3f18
missopi <missopi@me.com>
2022-08-21 11:12:46 +0100

    Create functions for temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(faranheit) {
+  let fTemp = faranheit;
+  let fToCel = (fTemp - 32) * 5/9; 
+  return fToCel;
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  let cTemp = celcius;
+  let cToFar = cTemp * 5/9 + 32;
+  return cToFar;
 };
 
 // Do not edit below this line
