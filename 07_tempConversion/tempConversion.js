ef81383a12fcd26811053466d044109a1dfb4398
missopi <missopi@me.com>
2022-08-21 11:17:08 +0100

    Round to one decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
 const ftoc = function(faranheit) {
   let fTemp = faranheit;
   let fToCel = (fTemp - 32) * 5/9; 
-  return fToCel;
+  let fRounded = Math.round(fToCel * 10) / 10;
+  return fRounded;
 };
 
 const ctof = function(celcius) {
   let cTemp = celcius;
   let cToFar = cTemp * 5/9 + 32;
-  return cToFar;
+  let cRounded = Math.round(cToFar * 10) / 10;
+  return cRounded;
 };
 
 // Do not edit below this line
