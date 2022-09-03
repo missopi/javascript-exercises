c74c323b3b3fb51e81b875e344398aad405ccbec
missopi <missopi@me.com>
2022-09-03 12:15:05 +0100

    Complete exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    re = /[\W_]/g;
+    let lowJoinStr = str.toLowerCase().replace(re, '');
+    let reverseStr = lowJoinStr.split('').reverse().join('');
+    return reverseStr === lowJoinStr;
 };
 
 // Do not edit below this line
