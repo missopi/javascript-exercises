0f9e043547d897a5d6644bb825381f59c174cc0b
missopi <missopi@me.com>
2022-09-03 11:40:46 +0100

    Complete exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  };
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.length ? array.reduce((accumulator, nextItem) => accumulator * nextItem): 0;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n-1);
+  };
 };
+
 
 // Do not edit below this line
 module.exports = {
